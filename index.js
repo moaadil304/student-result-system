010b20f8367bef115824b56d129469bf68725f91
Mohammad Aadil <233125@kit.ac.in>
2025-11-09 12:47:13 +0530

    Updated port for Render deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-let port = 8080;
+const port = process.env.PORT || 8080;
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname,"views"));
 app.use(express.static("public"));
