efc0482a672f05533c014657dd6474a6df225cf6
Mohammad Aadil <233125@kit.ac.in>
2025-11-29 17:17:56 +0530

    updated for Railway deployment

--- index.js
+++ index.js
@@ -21,13 +21,9 @@ app.use(express.static("public"));
 // });
 
 //for deployment
-const db = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
+
+
 
 
 // HOME LOGIN PAGE
