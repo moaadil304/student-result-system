8e2bff66592dc50d237de1f536158a7414e5d513
Mohammad Aadil <233125@kit.ac.in>
2025-12-02 00:11:38 +0530

    fix static css on vercel

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ require("dotenv").config();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 // DB connection (callback mode)
 // const db = mysql.createPool({
